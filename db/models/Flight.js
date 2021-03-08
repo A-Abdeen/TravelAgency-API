382255c1f05a9dfcd429afe26574bae8a4272d49
A-Abdeen <ahmed.yahya18@gmail.com>
2021-03-08 12:04:35 +0300

    add flight model

---
+++ db/models/Flight.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+  const Flight = sequelize.define("Flight", {
+    from: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    to: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    departureDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    arrivalDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    departureAirport: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    arrivalAirport: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    economySeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    economyPrice: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    businessSeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    businessPrice: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+  return Flight;
+};

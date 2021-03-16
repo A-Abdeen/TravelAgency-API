235bc5be7e66348f23d616fd72a6c71e8e01d74a
robie09 <you@example.com>
2021-03-16 22:38:09 +0300

    done with booking and passenger-model

--- db/models/Booking.js
+++ db/models/Booking.js
@@ -1,59 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
   const Booking = sequelize.define("Booking", {
-    email: {
-      type: DataTypes.STRING,
-      // allowNull: false,
-      unique: {
-        args: true,
-        msg: "Email address already used",
-      },
-      validate: {
-        isEmail: {
-          args: true,
-          msg: "Must be standard email format (email@domain.com)",
-        },
-      },
-    },
-    countryCode: {
-      type: DataTypes.STRING,
-      //allowNull: false,
-    },
-    phone: {
-      type: DataTypes.INTEGER,
-      // allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING,
-    },
-    firstName: {
+    fullName: {
       type: DataTypes.STRING,
       // allowNull: false,
       unique: {
         args: true,
         msg: "Username taken",
       },
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      //allowNull: false,
-    },
-    gender: {
-      type: DataTypes.STRING,
-    },
-    nationality: {
-      type: DataTypes.STRING,
-    },
-    passportNum: {
-      type: DataTypes.INTEGER,
-      //allowNull: false,
-    },
-    countryIssue: {
-      type: DataTypes.STRING,
-      //allowNull: false,
-    },
-    expiryDate: {
-      type: DataTypes.DATE,
-      //allowNull: false,
+      email: {
+        type: DataTypes.STRING,
+        // allowNull: false,
+        unique: {
+          args: true,
+          msg: "Email address already used",
+        },
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Must be standard email format (email@domain.com)",
+          },
+        },
+      },
+      countryCode: {
+        type: DataTypes.STRING,
+        //allowNull: false,
+      },
+      phone: {
+        type: DataTypes.INTEGER,
+        // allowNull: false,
+      },
     },
   });
 
